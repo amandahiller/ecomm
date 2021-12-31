fd9f132d6bdc8ac36396990dc45f0b7a01c4b4c3
Amanda Speroni-Hiller <amandasperoni@icloud.com>
2021-12-30 17:01:52 -0800

    fixed error

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -5,15 +5,7 @@ const router = express.Router();
 
 router.get('/signup', (req, res) => {
     res.send(`
-      <div>
-        Your id is: ${req.session.userId}
-        <form method="POST">
-          <input name="email" placeholder="email" />
-          <input name="password" placeholder="password" />
-          <input name="passwordConfirmation" placeholder="password confirmation" />
-          <button>Sign Up</button>
-        </form>
-      </div>
+      
     `);
   });
   
