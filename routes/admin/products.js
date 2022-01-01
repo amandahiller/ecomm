2a501b78abcc750459af80247b321b3d1700152f
Amanda Speroni-Hiller <amandasperoni@icloud.com>
2022-01-01 15:24:14 -0800

    wip

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,13 +1,20 @@
 const express = require('express');
-const { validationResult } = require('express-validator');
+const {
+    validationResult
+} = require('express-validator');
 const multer = require('multer');
 
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
-const { requireTitle, requirePrice } = require('./validators');
+const {
+    requireTitle,
+    requirePrice
+} = require('./validators');
 
 const router = express.Router();
-const upload = multer({ storage: multer.memoryStorage() });
+const upload = multer({
+    storage: multer.memoryStorage()
+});
 
 router.get('/admin/products', (req, res) => {
 
@@ -17,14 +24,18 @@ router.get('/admin/products/new', (req, res) => {
     res.send(productsNewTemplate({}));
 });
 
-router.post('/admin/products/new', [requireTitle, requirePrice], (req, res) => {
-    const errors = validationResult(req);
-    
-    req.on('data', data => {
-        console.log(data.toString());
-    });
+router.post('/admin/products/new', [requireTitle, requirePrice],
+    upload.single('image'),
+    (req, res) => {
+        const errors = validationResult(req);
 
-    res.send('submitted');
-});
+        console.log(req.file);
+
+        req.on('data', data => {
+            console.log(data.toString());
+        });
+
+        res.send('submitted');
+    });
 
 module.exports = router;
