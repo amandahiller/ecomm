3c05073dcd0f9dd463e7d9e3791a8788e1bab056
Amanda Speroni-Hiller <amandasperoni@icloud.com>
2022-01-05 15:23:47 -0800

    completed middleware

--- routes/admin/middlewares.js
+++ routes/admin/middlewares.js
@@ -3,7 +3,13 @@ const { validationResult } = require('express-validator');
 module.exports = {
     handleErrors(templateFunc) {
         return (req, res, next) => {
+            const errors = validationResults(req);
 
+            if (!errors.isEmpty()) {
+                return res.send(templateFunc({ errors }));
+            }
+
+          next();
         };
     }
 };
