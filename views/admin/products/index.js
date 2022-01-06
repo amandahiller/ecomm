eabd3c61c1aa5a12ca1b3b8b820d39a47f9dd920
Amanda Speroni-Hiller <amandasperoni@icloud.com>
2022-01-05 16:03:56 -0800

    adding HTML for products

--- views/admin/products/index.js
+++ views/admin/products/index.js
@@ -1,3 +1,16 @@
-module.exports = ({ }) => {
+const layout = require('../layout');
 
+module.exports = ({ products }) => {
+    const renderedProducts = products.map((product) => {
+        return `
+          <div>${product.title}</div>
+        `;
+    });
+
+    return layout({
+        content: `
+           <h1 class="title">Products</h1>
+
+        `
+    });
 };
